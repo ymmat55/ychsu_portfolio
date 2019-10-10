34f008f344feb093b71d62815f50ec8880bd64c4
YC <ymmat55@gmail.com>
2019-10-09 21:36:20 -0400

    pages created

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <h1>About YC</h1>
+      <div>
+        <h5>
+          Full stack web developer with 5+ years in marketing and public
+          relations. Professional experience in tech startups, educational
+          institute and news publishing houses. Enjoys collaborating with all
+          levels to gather information, solve problems and make impact.
+        </h5>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+const Index = () => {
+  return (
+    <div>
+      <h1>Hello.</h1>
+      <h2>I'm YC</h2>
+      <p>
+        Need a developer? <a href="/contact">Contact me.</a>
+      </p>
+      <p>
+        Need a developer?<Link to="/contact">Contact me.</Link>
+      </p>
+    </div>
+  )
+}
+
+export default Index
